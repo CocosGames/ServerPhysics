f86cb3341e95f94e9cf8e55372fd10fda528dd57
COCO <support@cocosgames.com>
2023-02-19 21:06:56 +0800

    state

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,16 +1,17 @@
 import {Client, Room} from "colyseus";
-import {MyRoomState} from "./schema/MyRoomState";
-import {b2BodyType, b2CircleShape, b2PolygonShape, b2Shape, b2ShapeType, b2Vec2, b2World} from "@box2d/core";
+import {MyRoomState, Player} from "./schema/MyRoomState";
+import {b2Body, b2BodyType, b2CircleShape, b2PolygonShape, b2Shape, b2ShapeType, b2Vec2, b2World} from "@box2d/core";
 import * as level from "./level.json";
 
 export class MyRoom extends Room<MyRoomState> {
 
     public world: b2World = null;
+    public playerBodies: Map<string, b2Body> = new Map<string,b2Body>();
 
 
     onCreate(options: any) {
         this.setState(new MyRoomState());
-
+        this.autoDispose = false;
         const gravity: b2Vec2 = new b2Vec2(0, -10);
         this.world = b2World.Create(gravity);
         const box: b2PolygonShape = new b2PolygonShape();
@@ -22,24 +23,20 @@ export class MyRoom extends Room<MyRoomState> {
             }).CreateFixture({shape: box});
         });
 
-
-        // this.world.CreateBody({type: b2BodyType.b2_dynamicBody, position:new b2Vec2(10,20)}).CreateFixture({
-        //     shape: new b2CircleShape(1),
-        //     density: 1
-        // });
-
-
         this.clock.setInterval(() => {
-            console.log("OnStep!");
-            // if (!this.world)
-            //   return;
             this.world.Step(1 / 60, {velocityIterations: 10, positionIterations: 10});
-            var pos = this.world.GetBodyList().GetPosition();
-            console.log(pos);
+            this.playerBodies.forEach((b,k,m)=>{
+                let pos = b.GetPosition();
+                let p = this.state.players.get(k);
+                if (p)
+                {
+                    p.x = pos.x;
+                    p.y = pos.y;
+                }
+            })
         }, 100);
 
         console.log("Room created!");
-
 
         this.onMessage("type", (client, message) => {
             //
@@ -51,10 +48,21 @@ export class MyRoom extends Room<MyRoomState> {
 
     onJoin(client: Client, options: any) {
         console.log(client.sessionId, "joined!");
+
+        let b = this.world.CreateBody({type: b2BodyType.b2_dynamicBody, position:new b2Vec2(20,20)});
+        b.SetFixedRotation(true);
+        b.CreateFixture({
+            shape: new b2CircleShape(1),
+            density: 1
+        });
+        this.state.players.set(client.sessionId, new Player());
+        this.playerBodies.set(client.sessionId, b);
     }
 
     onLeave(client: Client, consented: boolean) {
         console.log(client.sessionId, "left!");
+        this.state.players.delete(client.sessionId);
+        this.playerBodies.delete(client.sessionId);
     }
 
     onDispose() {
